0691d909cc17a987b0e3c66ad094ce061f105dd2
Aleeza Asghar <160777639+AleezaAsghar@users.noreply.github.com>
2025-09-04 13:25:11 +0500

    Update ffm.py

--- ffm.py
+++ ffm.py
@@ -7,7 +7,7 @@ import numpy as np
 # 1. Load ONE Model
 #    (Change filename when needed)
 # =========================
-MODEL_PATH = "ffm.pkl"  # <-- fixed path
+MODEL_PATH = "FFM.pkl"  # <-- fixed path
 with open(MODEL_PATH, "rb") as f:
     model = pickle.load(f)
 
